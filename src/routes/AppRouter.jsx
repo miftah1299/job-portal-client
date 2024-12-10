c6641a97291ea2d2bc5eeb15c81dfda957aa489f
miftah1299 <jannat27120@gmail.com>
2024-12-10 23:22:10 +0600

    initialize job portal project with Vite, React, and Tailwind CSS

---
+++ src/routes/AppRouter.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const AppRouter = createBrowserRouter([
+    {
+      path: "/",
+      element: <div>Hello world!</div>,
+    },
+  ]);
+
+  export default AppRouter;
