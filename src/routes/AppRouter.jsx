eef04224ccccdc520e41505efb3fa9d68379a3f1
miftah1299 <jannat27120@gmail.com>
2024-12-10 23:38:03 +0600

    add main layout, home, and not found pages

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,10 +1,20 @@
 import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../layouts/MainLayout";
+import Home from "../pages/Home/Home";
+import NotFound from "../pages/NotFound/NotFound";
 
 const AppRouter = createBrowserRouter([
     {
-      path: "/",
-      element: <div>Hello world!</div>,
+        path: "/",
+        element: <MainLayout />,
+        errorElement: <NotFound />,
+        children: [
+            {
+                path: "/",
+                element: <Home />,
+            },
+        ],
     },
-  ]);
+]);
 
-  export default AppRouter;
+export default AppRouter;
