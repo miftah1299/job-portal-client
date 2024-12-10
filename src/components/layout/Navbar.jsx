256ffe9be1eebdb0c7fe78b9c91078a653164a45
miftah1299 <miftahjannat25@gmail.com>
2024-12-11 02:02:41 +0600

    add authentication layout ,implement Navbar and Footer components

---
+++ src/components/layout/Navbar.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+    const links = (
+        <>
+            <li>
+                <Link to="/" className="btn btn-ghost">
+                    Home
+                </Link>
+                <Link to="/about" className="btn btn-ghost">
+                    About
+                </Link>
+                <Link to="/contact" className="btn btn-ghost">
+                    Contact
+                </Link>
+            </li>
+        </>
+    );
+    return (
+        <div className="navbar bg-background">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <div
+                        tabIndex={0}
+                        role="button"
+                        className="btn btn-ghost lg:hidden"
+                    >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="h-5 w-5"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M4 6h16M4 12h8m-8 6h16"
+                            />
+                        </svg>
+                    </div>
+                    <ul
+                        tabIndex={0}
+                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+                    >
+                        {links}
+                    </ul>
+                </div>
+                <a className="btn btn-ghost text-2xl text-textPrimary">
+                    JobPortal
+                </a>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1">{links}</ul>
+            </div>
+            <div className="navbar-end space-x-4">
+                <Link className="btn btn-outline text-primary">Register</Link>
+                <Link className="btn btn-outline text-primary">Sign in</Link>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
