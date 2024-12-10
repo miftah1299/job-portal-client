a4b4673516652aa0e93e8f1751ad63e5a090690b
miftah1299 <jannat27120@gmail.com>
2024-12-11 02:02:41 +0600

    add authentication layout ,implement Navbar and Footer components

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Navbar from "../components/layout/Navbar";
 
 const AuthLayout = () => {
     return (
         <div>
-            
+            <Navbar />
+
+            <Outlet />
         </div>
     );
 };
--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -2,6 +2,8 @@ import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../layouts/MainLayout";
 import Home from "../pages/Home/Home";
 import NotFound from "../pages/NotFound/NotFound";
+import AuthLayout from "../layouts/AuthLayout";
+import Register from "../pages/Auth/Register";
 
 const AppRouter = createBrowserRouter([
     {
@@ -12,6 +14,16 @@ const AppRouter = createBrowserRouter([
             {
                 path: "/",
                 element: <Home />,
+            },
+        ],
+    },
+    {
+        path: "auth",
+        element: <AuthLayout />,
+        children: [
+            {
+                path: "register",
+                element: <Register />,
             },
         ],
     },
