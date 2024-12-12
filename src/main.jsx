224bd3ebc829e102cafd0139af740fad71547a1a
miftah1299 <miftahjannat25@gmail.com>
2024-12-12 11:56:42 +0600

    add AuthProvider to manage user authentication state and integrate Firebase authentication

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import AppRouter from "./routes/AppRouter";
+import AuthProvider from "./provider/AuthProvider";
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
-        <RouterProvider router={AppRouter} />
+        <AuthProvider>
+            <RouterProvider router={AppRouter} />
+        </AuthProvider>
     </StrictMode>
 );
