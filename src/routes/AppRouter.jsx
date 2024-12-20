d10e182cc56946a6b4f0225e40c70a9727f5bf33
miftah1299 <miftahjannat25@gmail.com>
2024-12-21 00:56:00 +0600

    add ViewApplications page and update routing for MyApplications

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -8,7 +8,7 @@ import Signin from "../pages/Auth/Signin";
 import Jobdetails from "../pages/Jobs/Jobdetails";
 import ProtectedRoute from "./ProtectedRoute";
 import JobApply from "../pages/Jobs/JobApply";
-import MyApplications from "../pages/Jobs/MyApplications";
+import MyApplications from "../pages/Applications/MyApplications";
 import AddJob from "../pages/Jobs/AddJob";
 import MyPostedJobs from "../pages/Jobs/MyPostedJobs";
 
@@ -66,7 +66,7 @@ const AppRouter = createBrowserRouter([
                         <MyPostedJobs />
                     </ProtectedRoute>
                 ),
-            }
+            },
         ],
     },
     {
