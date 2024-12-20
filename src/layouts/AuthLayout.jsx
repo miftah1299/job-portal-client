8178f4d489face85876caea668415c1a26df339e
Your Name <jannat27120@gmail.com>
2024-12-20 14:45:05 +0600

    refactor JobApply component to improve job application handling; add toast notifications

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
-import { Toaster } from "react-hot-toast";
 import Footer from "../components/Footer";
+import { Toaster } from "react-hot-toast";
 
 const AuthLayout = () => {
     return (
