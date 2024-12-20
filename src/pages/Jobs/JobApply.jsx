1a43abc0064fa54271e195b0f086e680922b49e3
miftah1299 <miftahjannat25@gmail.com>
2024-12-20 14:45:05 +0600

    refactor JobApply component to improve job application handling; add toast notifications

--- src/pages/Jobs/JobApply.jsx
+++ src/pages/Jobs/JobApply.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import toast from "react-hot-toast";
 import { useLoaderData, useParams } from "react-router-dom";
 
 const JobApply = () => {
     const { title } = useLoaderData();
     const { id } = useParams();
 
-    const handleJobApply = (e) => {
+    const handleJobApplication = (e) => {
         e.preventDefault();
 
         const form = e.target;
@@ -14,6 +15,7 @@ const JobApply = () => {
         const resume = form.resume.value;
         const coverLetter = form.coverLetter.value;
         // console.log({ linkedin, github, resume, coverLetter });
+
         const jobApplication = {
             job_id: id,
             application_email: UserActivation.email,
@@ -22,15 +24,32 @@ const JobApply = () => {
             resume,
             coverLetter,
         };
+
+        fetch("http://localhost:5000/job-applications", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(jobApplication),
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                console.log(data);
+                toast.success("Application submitted successfully");
+                // form.reset();
+            })
+            .catch((error) => {
+                console.error("Error:", error);
+            });
     };
-    
+
     return (
         <div className="py-20 bg-background">
             <div className="max-w-2xl mx-auto p-6 bg-base-100 rounded-lg shadow-lg">
                 <h1 className="text-2xl text-primaryLight font-bold mb-4">
                     Apply for {title}
                 </h1>
-                <form onSubmit={handleJobApply}>
+                <form onSubmit={handleJobApplication}>
                     <div className="mb-4">
                         <label
                             className="block text-sm font-medium mb-2"
