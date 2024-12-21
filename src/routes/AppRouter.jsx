938bb0c306b5f43a830b9fd7dc286b8686fadd1a
miftah1299 <jannat27120@gmail.com>
2024-12-22 05:15:21 +0600

    update API endpoints to use production server URLs

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -30,7 +30,7 @@ const AppRouter = createBrowserRouter([
                     </ProtectedRoute>
                 ),
                 loader: ({ params }) =>
-                    fetch(`http://localhost:5000/jobs/${params.id}`),
+                    fetch(`https://job-portal-server-site1.vercel.app/jobs/${params.id}`),
             },
             {
                 path: "jobs-apply/:id",
@@ -40,7 +40,7 @@ const AppRouter = createBrowserRouter([
                     </ProtectedRoute>
                 ),
                 loader: ({ params }) =>
-                    fetch(`http://localhost:5000/jobs/${params.id}`),
+                    fetch(`https://job-portal-server-site1.vercel.app/jobs/${params.id}`),
             },
             {
                 path: "my-applications",
@@ -77,7 +77,7 @@ const AppRouter = createBrowserRouter([
                 ),
                 loader: ({ params }) =>
                     fetch(
-                        `http://localhost:5000/job-applications/jobs/${params.job_id}`
+                        `https://job-portal-server-site1.vercel.app/job-applications/jobs/${params.job_id}`
                     ),
             },
         ],
