0ecc0cce175881094ec8d5223feb02d3eb4b829b
miftah1299 <miftahjannat25@gmail.com>
2024-12-23 01:27:19 +0600

    implement useAxiosSecure hook for secure API requests and update endpoints to use production server URLs

--- src/pages/Applications/ViewApplications.jsx
+++ src/pages/Applications/ViewApplications.jsx
@@ -9,7 +9,7 @@ const ViewApplications = () => {
         const data = {
             status: e.target.value,
         };
-        fetch(`http://localhost:5000/job-applications/${id}`, {
+        fetch(`https://jobportal-server-side.vercel.app/job-applications/${id}`, {
             method: "PATCH",
             headers: {
                 "Content-Type": "application/json",
